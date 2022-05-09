21a181b216759b2e7b2e0b70f00274033f4d7648
Elizabevil <2197377595@qq.com>
2022-05-08 23:45:19 -0400

    tools

--- payDemo/src/main/java/com/wechatpay/utils/BAse.kt
+++ payDemo/src/main/java/com/wechatpay/utils/BAse.kt
@@ -6,5 +6,13 @@ package com.wechatpay.utils
  * @Time    : 5/8/22
  * https://www.jianshu.com/p/a5e15f2f2ed1
  */
-class BAse {
+class BaseInfo {
+    //appid 微信分配的公众账号ID
+    val APP_ID = ""
+
+    //商户号 微信分配的公众账号ID
+    val MCH_ID = ""
+
+    //  API密钥，在商户平台设置
+    val API_KEY = ""
 }
